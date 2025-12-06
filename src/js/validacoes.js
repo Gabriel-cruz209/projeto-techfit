be060d789ff9c872ea46f6adedc8864044ac3e7e
VictorHCamargo <victor.h0786@gmail.com>
2025-12-06 12:44:56 -0300

    ajustes - VHC

--- src/js/validacoes.js
+++ src/js/validacoes.js
@@ -4,24 +4,24 @@ const form = document.getElementById("form_validacao");
 form.addEventListener("submit", function (e) {
 
     const campos = form.querySelectorAll("input");
-
+    console.log(campos)
     for (let campo of campos) {
         
-        if (campo.name === "cpf" && !regexCPF.test(campo.value)) {
+        if (campo.id == "cpf" && !regex_cpf.test(campo.value)) {
             e.preventDefault();
             alert("CPF inválido!");
             campo.focus();
             return;
         }
 
-        if (campo.name === "cep" && !regexCEP.test(campo.value)) {
+        if (campo.id == "cep" && !regex_cep.test(campo.value)) {
             e.preventDefault();
             alert("CEP inválido!");
             campo.focus();
             return;
         }
 
-        if (campo.name === "telefone" && !regexTelefone.test(campo.value)) {
+        if (campo.id == "telefone" && !regex_telefone.test(campo.value)) {
             e.preventDefault();
             alert("Telefone inválido!");
             campo.focus();
